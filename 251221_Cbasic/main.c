f288f495500b9f629d98edbd964813a49b5b6f9b
yeajieo <like.crong@gmail.com>
2025-12-21 21:55:28 +0900

    prettier

--- 251221_Cbasic/main.c
+++ 251221_Cbasic/main.c
@@ -1,6 +1,6 @@
-/*  
+/*
     강의 내용 : https://www.youtube.com/watch?v=xND0t1pr3KY
-    
+
     1. 환경 설정 : c runner, c extension pack 설치
     터미널 열기
     c coplier 버전 확인, clang --version
@@ -28,10 +28,10 @@ int main()
     printf("new gpa is %.1f\n", gpa);
     printf("new gpa is %.2f\n", gpa);
 
-    double longnum = 1.123456789012345; // 8 byte
-    printf("i wanna know the num %lf.\n", longnum); //1.1234567
-    printf("i wanna know the num %.10lf.\n", longnum); //1.1234567890
-    printf("i wanna know the num %.15lf.\n", longnum); //1.123456789012345
+    double longnum = 1.123456789012345;                // 8 byte
+    printf("i wanna know the num %lf.\n", longnum);    // 1.1234567
+    printf("i wanna know the num %.10lf.\n", longnum); // 1.1234567890
+    printf("i wanna know the num %.15lf.\n", longnum); // 1.123456789012345
 
     char v1 = 'a'; // 1 byte
     printf("new word '%c'!\n", v1);
@@ -39,21 +39,21 @@ int main()
     // printf("new word '%c'!\n", v2);
 
     char longcha[] = "long sentense!";
-    printf("let me say %s\n", longcha); 
+    printf("let me say %s\n", longcha);
 
     // 1 byte
-    bool online = true; //1
-    bool newone = false; //0
+    bool online = true;  // 1
+    bool newone = false; // 0
     printf("let know the result %d\n", online);
-    if (online) {
+    if (online)
+    {
         printf("end");
     }
-    else {
+    else
+    {
         printf("go");
     }
-
 
     return 0;
     // 0은 정상적으로 종료, 이외의 숫자 에러
 }
- 
