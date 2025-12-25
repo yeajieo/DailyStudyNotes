6e27d40db07dae6e25a9f42619a00a077c7ff240
yeajieo <like.crong@gmail.com>
2025-12-25 23:05:51 +0900

    add prettier

--- 251221_Cbasic/251225_02_condition/condition.c
+++ 251221_Cbasic/251225_02_condition/condition.c
@@ -10,7 +10,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (){
+int main()
+{
 
     // 1. arithmetic operaters
 
@@ -31,7 +32,7 @@ int main (){
 
     z2 = x2 / y2; // 0.000000 -> (float) = (int) / (int) : impossible
     printf("result : %f \n", z2);
-    
+
     int x3 = 2;
     float y3 = 3;
     float z3 = 0;
@@ -39,8 +40,8 @@ int main (){
     z3 = x3 / y3; // 0.000000 -> (float) = (int) / (float) : possible
     printf("result : %f \n", z3);
 
-    x++; // 10 + 1
-    x--; // (10 + 1) - 1
+    x++;    // 10 + 1
+    x--;    // (10 + 1) - 1
     x += 2; // ((10 + 1) - 1) +2
     x -= 2; // (// ((10 + 1) - 1) +2) -2
     x *= 3;
@@ -54,23 +55,21 @@ int main (){
     float gpa = 0.0f;
     char grade = '\0';
     char name[30] = "";
-    
+
     scanf("%d", &age);
-    scanf("%f", &gpa); //3.13333 + \n
+    scanf("%f", &gpa);    // 3.13333 + \n
     scanf(" %c", &grade); // _%c : \n이 grade에 자동으로 입력되는 것을 방지
-    
+
     getchar(); // clear '\n'
-    //scanf("%s", &age); // abc defg 입력 시, _에 의해 abc만 저장
+    // scanf("%s", &age); // abc defg 입력 시, _에 의해 abc만 저장
     fgets("%s", sizeof(name), stdin);
-    name[strlen(name)-1] = '\0'; // 마지막 칸 내용을 \n -> \0 으로 변경
+    name[strlen(name) - 1] = '\0'; // 마지막 칸 내용을 \n -> \0 으로 변경
     // strlen 은 string.h 필요
 
     printf("name : %s \n", name);
     printf("age : %d \n", age);
     printf("gpa : %f \n", gpa);
     printf("grade : %c \n", grade);
-    
+
     return 0;
-
 }
-
